9b97f98d89dba74941328b634474ca022c7dea85
kki0106 <kki190206@gmail.com>
2018-12-14 02:17:15 +0900

    Implement asynchronous communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,43 @@
 $(function(){
+  function buildHTML(message){
+    var image = message.image ? `<img src="${ message.image }" class="posts__image" />` : "";
+    var html = `<div class ="posts">
+                  <ul class ="posts__lists">
+                    <li class ="posts__lists--name">
+                      ${ message.user_name }
+                    </li>
+                    <li class ="posts__lists--time">
+                      ${ message.time }
+                    </li>
+                  <ul>
+                  <p class ="posts__message">
+                    ${ message.content }
+                  </p>
+                  ${ image }
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
-  });
-})
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-screen').append(html);
+      $('.post-form__message-form').val('')
+      $('.post-form__send-button').prop('disabled', false);
+      $('.chat-screen').animate({ scrollTop: $('.chat-screen')[0].scrollHeight }, 1000,);
+    })
+    .fail(function(){
+        alert('error');
+    })
+  })
+});
