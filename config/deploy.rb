757c608625673c96c7476f619a4ae288263fdb48
kki0106 <kki190206@gmail.com>
2018-12-18 03:56:43 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,6 @@ set :default_env, {
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
-set :linked_files, %w{ config/secrets.yml }
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
